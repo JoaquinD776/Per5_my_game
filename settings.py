d2db2165a0997eea9f75f591a8f45997d4596af0
JoaquinD776 <quino_d@icloud.com>
2024-10-04 10:56:19 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+FPS = 50
+TILESIZE = 32
+
+WIDTH = 1024
+HEIGHT = 768
+
+
+# colors
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+PINK = (255, 155, 155)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
